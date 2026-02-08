070a1260b7dede0f3746f954024e1418c60a61b1
feijaoozin <gonardo1234@gmail.com>
2026-02-08 00:53:59 -0300

    feat: começando a tela de detalhes do personagem

--- src/screens/CharacterScreen.tsx
+++ src/screens/CharacterScreen.tsx
@@ -1,9 +1,38 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { RouteProp } from "@react-navigation/native";
 
-export function CharacterScreen() {
+import { getCharacter } from "../services/rickAndMorty";
+import { Character } from "../types/character";
+
+type Props = {
+  route: RouteProp<{ params: { id: number } }, "params">;
+};
+
+export function CharacterScreen({ route }: Props) {
+  const { id } = route.params;
+  const [character, setCharacter] = useState<Character | null>(null);
+
+  useEffect(() => {
+    async function load() {
+      const data = await getCharacter(String(id));
+      setCharacter(data);
+    }
+    load();
+  }, [id]);
+
+  if (!character) return null;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Detalhes do personagem</Text>
+      <Image source={{ uri: character.image }} style={styles.image} />
+
+      <Text style={styles.name}>{character.name}</Text>
+
+      <View style={styles.card}>
+        <Text>Status: {character.status}</Text>
+        <Text>Espécie: {character.species}</Text>
+      </View>
     </View>
   );
 }
@@ -11,10 +40,25 @@ export function CharacterScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#F5F5F5",
+    padding: 16,
   },
-  text: {
-    fontSize: 18,
+  image: {
+    width: "100%",
+    height: 280,
+    borderRadius: 20,
+    marginBottom: 16,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 12,
+    textAlign: "center",
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    padding: 16,
+    elevation: 4,
   },
 });
