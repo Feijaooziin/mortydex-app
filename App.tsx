e0f3069023000f68df37c1fa96e2b71d229c0cfb
feijaoozin <gonardo1234@gmail.com>
2026-02-09 17:05:48 -0300

    feat: mudando o header

--- App.tsx
+++ App.tsx
@@ -5,22 +5,57 @@ import { HomeScreen } from "./src/screens/HomeScreen";
 import { CharacterScreen } from "./src/screens/CharacterScreen";
 import { RootStackParamList } from "./src/types/navigation";
 
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { View, Text } from "react-native";
+import { getCharacter } from "./src/services/rickAndMorty";
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#2ECC71",
+          },
+          headerTintColor: "#FFF",
+          headerTitleStyle: {
+            fontWeight: "900",
+          },
+        }}
+      >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "MortyDex APP" }}
+          options={{
+            headerTitle: () => (
+              <View
+                style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
+              >
+                <MaterialCommunityIcons
+                  name="flask-outline"
+                  size={26}
+                  color="#FFF"
+                />
+                <Text
+                  style={{
+                    color: "#FFF",
+                    fontSize: 18,
+                    fontWeight: "700",
+                  }}
+                >
+                  MORTY DEX
+                </Text>
+              </View>
+            ),
+          }}
         />
 
         <Stack.Screen
           name="Character"
           component={CharacterScreen}
-          options={{ title: "Details" }}
+          options={{ title: "Character Details" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
